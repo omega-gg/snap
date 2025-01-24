57784d87714419109fd26ce68c6cb219fd2d0fb5
bunjee <bunjee@omega.gg>
2025-01-24 10:34:34 +0100

    VLC 4.0.0

--- script/vlc.sh
+++ script/vlc.sh
@@ -7,34 +7,61 @@ set -e
 
 deploy="../deploy"
 
-snap="https://api.snapcraft.io/api/v1/snaps/download/RT9mcUhVsRYrDLG8qnvGiy26NKvv6Qkd_3078.snap"
+VLC_versionA="3.0.21"
+VLC_versionB="4.0.0"
+
+snapA="https://api.snapcraft.io/api/v1/snaps/download/RT9mcUhVsRYrDLG8qnvGiy26NKvv6Qkd_3777.snap"
+snapB="https://api.snapcraft.io/api/v1/snaps/download/RT9mcUhVsRYrDLG8qnvGiy26NKvv6Qkd_4070.snap"
 
 #--------------------------------------------------------------------------------------------------
-# Download
+# Functions
 #--------------------------------------------------------------------------------------------------
 
-echo "DOWNLOADING"
+download()
+{
+    echo "DOWNLOADING $1"
+    echo "$2"
 
-curl -L -o data.snap "$snap"
+    curl -L -o data.snap "$2"
+
+    echo "EXTRACTING"
+
+    unsquashfs -f -d data data.snap
+
+    echo "DEPLOYING"
+
+    mkdir -p $deploy/$1
+
+    mv data/* $deploy/$1
+
+    rm -rf data
+
+    echo ""
+}
 
 #--------------------------------------------------------------------------------------------------
-# Extract
+# List
 #--------------------------------------------------------------------------------------------------
 
-echo "EXTRACTING"
+echo "LISTING"
 
-rm -rf data
+curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/vlc
 
-unsquashfs -f -d data data.snap
+echo ""
 
 #--------------------------------------------------------------------------------------------------
-# Deploy
+# Clean
 #--------------------------------------------------------------------------------------------------
 
-echo "DEPLOYING"
+echo "CLEANING"
 
 rm -rf deploy/*
 
 touch $deploy/.gitignore
 
-mv data/* $deploy
+#--------------------------------------------------------------------------------------------------
+# Download
+#--------------------------------------------------------------------------------------------------
+
+download $VLC_versionA "$snapA"
+download $VLC_versionB "$snapB"
